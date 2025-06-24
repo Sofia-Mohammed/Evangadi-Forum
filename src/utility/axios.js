7f2aa1df324f75044ecce8239ed608818618f2b5
Sofi391-M <soforg37@gmail.com>
2025-06-23 19:46:23 -0700

    axios added

--- src/utility/axios.js
+++ src/utility/axios.js
@@ -1,13 +1,6 @@
-// import axios from "axios";
-
-// export const axiosInstance = axios.create({
-//   //local endpoint reference
-//   baseURL: `http://localhost:5000/api/v1`,
-// });
-// axios.js
 import axios from "axios";
 
-// Changed baseURL to cover all /api routes, assuming your auth routes are under /api/auth
+// Changed baseURL to cover all /api routes, assuming the auth routes are under /api/auth
 export const axiosInstance = axios.create({
   baseURL: `http://localhost:5000/api/v1`,
 });
